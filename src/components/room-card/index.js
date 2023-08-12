05257bdbaf4c6159ce8b0152e2bbc067ab3c3143
April Ai <sichun.ai@gmail.com>
2023-08-12 19:53:44 -0400

    add styling, and functionality

--- src/components/room-card/index.js
+++ src/components/room-card/index.js
@@ -1,10 +1,20 @@
-const RoomCard = ({ room }) => {
+import "./styles.css";
+
+const RoomCard = ({ room, bookRoom }) => {
   const { name, spots, thumbnail } = room;
+
   return (
     <div className="room-card-container">
-      <img src={thumbnail} alt={`${name}`} />
-      <div>{name}</div>
-      <div>{spots} spots remaining</div>
+      <img className="room-thumbnail" src={thumbnail} alt={`${name}`} />
+      <div className="room-booking-container">
+        <div className="room-name">{name}</div>
+        {spots !== 0 && (
+          <button className="room-book-btn" onClick={() => bookRoom(name)}>
+            Book!
+          </button>
+        )}
+      </div>
+      <div className="room-spots">{spots} spots remaining</div>
     </div>
   );
 };
--- src/routes/rooms/index.js
+++ src/routes/rooms/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import RoomCard from "../../components/room-card/index";
-import "../../index.css";
+import "./styles.css";
 
 const Rooms = () => {
   const [roomsArray, setRoomsArray] = useState([]);
@@ -14,16 +14,34 @@ const Rooms = () => {
     getRoomsData();
   }, []);
 
+  const handleOnBookRoom = (roomName) => {
+    alert(roomName + " has been booked!");
+    const updatedRooms = roomsArray.map((room) => {
+      if (room.name === roomName) {
+        return {
+          name: room.name,
+          spots: room.spots - 1,
+          thumbnail: room.thumbnail,
+        };
+      } else {
+        return {
+          ...room,
+        };
+      }
+    });
+    setRoomsArray(updatedRooms);
+  };
+
   return (
     <div className="rooms-container">
-      <h1>Rooms</h1>
-      <h2>
+      <h1 className="rooms-header">Rooms</h1>
+      <h2 className="rooms-description">
         Odio nisi, lectus dis nulla. Ultrices maecenas vitae rutrum dolor
         ultricies donec risus sodales. Tempus quis et.
       </h2>
       <div className="rooms-grid">
         {roomsArray.map((room) => (
-          <RoomCard key={room.name} room={room} />
+          <RoomCard key={room.name} room={room} bookRoom={handleOnBookRoom} />
         ))}
       </div>
     </div>
